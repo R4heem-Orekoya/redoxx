edf2eae258af76c32679301687b1e113dfa6586f
Raheem Orekoya <tundeorekoya1@gmail.com>
2024-08-27 05:09:29 +0100

    fixed some linting issue

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -11,24 +11,24 @@ const About = () => {
             <div className="col-span-1 md:col-span-4">
                <div className="grid gap-4 max-sm:text-sm text-muted-foreground">
                   <p>
-                     Hey! I'm Raheem, a full-stack developer from Nigeria 
+                     Hey! I&apos;m Raheem, a full-stack developer from Nigeria 
                      with a passion for coding and mathematics. I specialize 
-                     in backend development using Node.js and Express, but I'm 
+                     in backend development using Node.js and Express, but I&apos;m 
                      also proficient in frontend technologies like React and 
                      Next.js. This allows me to tackle projects from start to 
                      finish with a comprehensive understanding of both client-side 
                      and server-side development.
                   </p>
                   <p>
-                     I've honed my skills through continuous learning, coding 
+                     I&apos;ve honed my skills through continuous learning, coding 
                      projects, and tackling complex mathematical problems. 
-                     I'm currently seeking new opportunities to leverage my 
+                     I&apos;m currently seeking new opportunities to leverage my 
                      technical expertise and contribute to innovative projects.
                   </p>
                   <p>
-                     If you're looking for a skilled developer with a strong foundation in 
+                     If you&apos;re looking for a skilled developer with a strong foundation in 
                      both backend and frontend development and a passion for problem-solving, 
-                     let's connect!
+                     let&apos;s connect!
                   </p>
                </div>
                <div className="flex items-center gap-6 mt-8">
@@ -38,13 +38,13 @@ const About = () => {
                   </p>
                   <ul className="flex items-center gap-4">
                      {Socials.map((social) => (
-                        <li><a href={social.link}>{social.icon}</a></li>
+                        <li key={social.title}><a href={social.link}>{social.icon}</a></li>
                      ))}
                   </ul>
                </div>
             </div>
             <div className="col-span-1 md:col-span-2 grid gap-8">
-               {Tools.map((tool, i) => (
+               {Tools.map((tool) => (
                   <div key={tool.title}>
                      <h2 className="flex items-center gap-2">
                         <div className="p-1 bg-white w-fit grid place-items-center rounded-sm">
--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
             <p className="text-center max-sm:text-sm mt-4 text-muted-foreground">
                Shoot me an email if you want to connect! 
                You can also find me on <span className="text-white font-medium hover:underline"><a href="#">Twitter</a></span> or <span className="text-white font-medium hover:underline"><a href="#">Instagram </a></span> 
-               if that's more your speed.
+               if that&apos;s more your speed.
             </p>
             <a href="mailto:raheemorekoya1@gmail.com" className="flex items-center justify-center gap-2 mt-4 opacity-70 hover:opacity-100 max-sm:text-sm">
                <Mail strokeWidth={1.5} className="flex-shrink-0 max-sm:w-4 max-sm:h-4"/>
