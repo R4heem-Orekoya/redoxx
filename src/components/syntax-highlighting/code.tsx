fb936e10e34113850ace737a93088f0049c77402
Redoxx <112984523+R4heem-Orekoya@users.noreply.github.com>
2025-02-02 23:49:15 +0100

    Update code.tsx

--- src/components/syntax-highlighting/code.tsx
+++ src/components/syntax-highlighting/code.tsx
@@ -5,7 +5,7 @@ const CodeBlock = async ({ code, language }: { code: string, language: string })
    const lang = language === "golang" ? "go" : language
    const html = await codeToHtml(code, {
       lang,
-      theme: 'tokyo-night', //one-dark-pro, tokyo-night, poimandres
+      theme: 'dark-plus', //one-dark-pro, tokyo-night, poimandres, tokyo-night
    });
 
    return (
